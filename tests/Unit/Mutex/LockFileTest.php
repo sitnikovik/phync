9df60931efcadd1896aac6a27070349d7bfc2d6f
Ilya Sitnikov <sitnikovik@gmail.com>
2025-05-21 06:41:46 +0300

    chore: add copyright notice to skipped

--- tests/Unit/Mutex/LockFileTest.php
+++ tests/Unit/Mutex/LockFileTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Ilya Sitnikov <sitnikovik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Sitnikovik\Phync\Test\Unit\Mutex;
 
 use RuntimeException;
